6694ff3448e074b95532ac221d969a9d4d576c06
Abdelrahman Tarek Elabrak <abdelrahmantelabrak@gmail.com>
2024-05-19 00:46:57 +0300

    isLate Function

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -6,3 +6,13 @@ String todayDate() {
   String formatted = formatter.format(now);
   return formatted;
 }
+
+bool isLate(String formattedDate){
+  DateTime currentDate = DateTime.now();
+  DateFormat formatter = DateFormat('MMMM dd, yyyy');
+  String formatted = formatter.format(currentDate);
+  // Parse the two dates
+  DateTime date = DateFormat('MMMM dd, yyyy').parse(formattedDate);
+  return date.isBefore(currentDate) && formattedDate!=formatted;
+
+}
